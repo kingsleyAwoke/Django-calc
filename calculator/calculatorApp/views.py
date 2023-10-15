bf58f0e2048f093721bb39903eb65c266b4ed9ad
Kingsley <kingsley_social@yahoo.com>
2023-10-16 00:06:49 +0100

    Added the program code

--- calculator/calculatorApp/views.py
+++ calculator/calculatorApp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+
+def result(request):
+    num1 = int(request.GET.get('number1'))
+    num2 = int(request.GET.get('number2'))
+
+    if request.GET.get('add') == "":
+        ans = num1 + num2
+    
+    elif request.GET.get('subtract') == "":
+        ans = num1 - num2
+
+    elif request.GET.get('multiply') == "":
+        ans = num1 * num2
+
+    elif request.GET.get('divide') == "":
+        ans = round(num1 / num2)
+
+    return render(request, 'result.html', {'ans': ans})
